9646460a6c7479d2447ccd08a7d0232dae3703c0
VersaHVC <versahvc@gmail.com>
2023-03-06 16:49:04 +0000

    Update BigBar.cs

--- TheMixManUnityFile/Assets/Scripts/BigBar.cs
+++ TheMixManUnityFile/Assets/Scripts/BigBar.cs
@@ -10,19 +10,82 @@ public class BigBar : MonoBehaviour
 
     RectTransform l_I_Rect;
     RectTransform r_I_Rect;
+    RectTransform goalRect;
+
+
+    float max_L_I_Prog;
+    float max_R_I_Prog;
+
+    float current_L_I_Prog;
+    float current_R_I_Prog;
+
+    float l_I_Overshoot;
+    float r_I_Overshoot;
     // Start is called before the first frame update
     void Start()
     {
         l_I_Rect = l_I_Prog.GetComponent<RectTransform>(); 
         r_I_Rect = r_I_Prog.GetComponent<RectTransform>(); 
+        goalRect = goal.GetComponent<RectTransform>();
+
+        max_L_I_Prog = goalRect.anchoredPosition.x + goalRect.pivot.x  * goalRect.rect.width;
+        max_R_I_Prog = goalRect.anchoredPosition.x + goalRect.pivot.x  * goalRect.rect.width;
+
+        current_L_I_Prog = l_I_Rect.rect.width;
+        current_R_I_Prog = r_I_Rect.rect.width;
+
+        l_I_Overshoot = 0;
+        r_I_Overshoot = 0;
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckBoundaries();
+        CheckOvershoot();
+        
+
         if (Input.GetKeyDown(KeyCode.Q))
-            l_I_Rect.sizeDelta = new Vector2(l_I_Rect.rect.width + 10, l_I_Rect.rect.height);
-        else if(Input.GetKeyDown(KeyCode.E))
-            r_I_Rect.sizeDelta = new Vector2(r_I_Rect.rect.width + 10, r_I_Rect.rect.height); ;
+        {
+            l_I_Rect.sizeDelta = new Vector2(l_I_Rect.rect.width + 5, l_I_Rect.rect.height);
+            current_L_I_Prog = l_I_Rect.rect.width;
+        }            
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            r_I_Rect.sizeDelta = new Vector2(r_I_Rect.rect.width + 5, r_I_Rect.rect.height);
+            current_R_I_Prog = r_I_Rect.rect.width;
+        }
+             
+    }
+
+    void CheckOvershoot()
+    {
+        if (current_L_I_Prog > max_L_I_Prog)
+        {
+            l_I_Overshoot = current_L_I_Prog - max_L_I_Prog;
+
+            Debug.Log(l_I_Overshoot);
+        }
+
+        if (current_R_I_Prog > max_R_I_Prog)
+        {
+            r_I_Overshoot = current_R_I_Prog - max_R_I_Prog;
+            Debug.Log(r_I_Overshoot);
+        }
+    }
+
+    void CheckBoundaries()
+    {
+        if(l_I_Rect.rect.width >= 100)
+        {
+            l_I_Rect.sizeDelta = new Vector2(99, l_I_Rect.rect.height);
+            r_I_Rect.sizeDelta = new Vector2(1, r_I_Rect.rect.height);
+        }else if(r_I_Rect.rect.width >= 100)
+        {
+            l_I_Rect.sizeDelta = new Vector2(1, l_I_Rect.rect.height);
+            r_I_Rect.sizeDelta = new Vector2(99, r_I_Rect.rect.height);
+        }
     }
 }
