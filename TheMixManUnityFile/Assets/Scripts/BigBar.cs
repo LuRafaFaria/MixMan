7c9e82d0e921304e68e0e8fe840198a3ce8db12a
VersaHVC <versahvc@gmail.com>
2023-03-06 15:51:51 +0000

    added bars

---
+++ TheMixManUnityFile/Assets/Scripts/BigBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBar : MonoBehaviour
+{
+    [SerializeField]GameObject l_I_Prog;
+    [SerializeField]GameObject r_I_Prog;
+    [SerializeField]GameObject goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
